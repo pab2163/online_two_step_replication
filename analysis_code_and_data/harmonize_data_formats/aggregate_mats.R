11f5cf843c75dac590cfee1e01e6ee70e177cfba
Paul A. Bloom <pab2163@columbia.edu>
2022-01-17 18:06:00 -0500

    aggregate decker & potter mats to csv
    no data cleaning yet

---
+++ analysis_code_and_data/harmonize_data_formats/aggregate_mats.R
@@ -0,0 +1,18 @@
+source('harmonize_data_formats/read_one_participant_mat.R')
+library(R.matlab)
+library(tidyverse)
+
+
+decker_files = Sys.glob('data/decker/decker_mat_files/*.mat')
+potter_files = Sys.glob('data/potter/potter_mats/*.mat')
+
+decker_df = decker_files %>% 
+  map_dfr(read_one_participant, total_trials = 200)
+
+potter_df = potter_files %>% 
+  map_dfr(read_one_participant, total_trials = 150)
+
+
+write.csv(decker_df, file = 'data/decker/combined_decker_data.csv', row.names = FALSE)
+write.csv(potter_df, file = 'data/decker/combined_potter_data.csv', row.names = FALSE)
+
